a69bf629d6084c49b64af0a2ba17faa39aa9edc6
chemicalsp4rk <163674404+chemicalsp4rk@users.noreply.github.com>
2025-07-05 18:28:17 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const clientId = "489e0479f85e42548d4dcaddcd0d3dfb";
+const redirectUri = "https://chemicalsp4rk.github.io/Aethyrnox/";
+const scopes = "user-top-read";
+
+window.onload = () => {
+  const hash = window.location.hash;
+  let token = null;
+
+  if (hash) {
+    const params = new URLSearchParams(hash.substring(1));
+    token = params.get("access_token");
+    window.history.pushState("", document.title, window.location.pathname);
+    if (token) {
+      localStorage.setItem("spotify_token", token);
+      window.location.href = "main.html";
+    }
+  }
+};
